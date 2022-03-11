3303b4fc864750715875b680c14af9adc66dc43e
Lê Long Vũ <lgv17121@gmail.com>
2022-03-11 21:51:07 +0700

    DangThanhTung + HoangLeTung

---
+++ root/header.php
@@ -0,0 +1,12 @@
+<section class="home">
+
+<div class="header-top">
+  <div class="search">
+    <input type="text" class="searchTerm" placeholder="What are you looking for?">
+    <button type="submit" class="searchButton">
+      <i class="fa fa-search"></i>
+    </button>
+  <div class="img-user"><span><img width = "50" height = "50" src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png" alt=""></span></div>
+</div>
+
+</div>
