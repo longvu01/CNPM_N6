960770e9a963aef0dc32a77e487ee456f7353b9b
quangthanhtran <tq1191703@gmail.com>
2022-03-20 22:30:17 +0700

    sss

--- root/menu.php
+++ root/menu.php
@@ -106,7 +106,7 @@ mysqli_close($conn);
 
     <div class="bottom-content">
       <li class="">
-        <a href="login.php">
+        <a href="../logout.php">
           <i class="bx bx-log-out icon"></i>
           <span class="text nav-text">Logout</span>
         </a>
