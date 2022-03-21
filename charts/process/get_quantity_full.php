d4442a24a250797c6e9227cb38437791a26e7015
Lê Long Vũ <lgv17121@gmail.com>
2022-03-21 11:07:18 +0700

    update

--- charts/process/get_quantity_full.php
+++ charts/process/get_quantity_full.php
@@ -15,7 +15,7 @@ if ($today < $max_date) {
   $last_month = date('m', strtotime(" -1 month"));
   $last_month_date = date('Y-m-d', strtotime(" -1 month"));
   $max_day_of_last_month = (new DateTime($last_month_date))->format('t');
-  $start_day_of_last_month = $max_day_of_last_month - $qty_day_last_month;
+  $start_day_of_last_month = $max_day_of_last_month - $qty_day_last_month + 1;
 
   for ($i = $start_day_of_last_month; $i <= $max_day_of_last_month; ++$i) {
     $key = $i . '-' . $last_month;
@@ -24,12 +24,12 @@ if ($today < $max_date) {
 
   $start_day_of_cur_month = 1;
 } else {
-  $start_day_of_cur_month = $today - $max_date;
+  $start_day_of_cur_month = $today - $max_date + 1;
 }
 
 // Current month
 $cur_month = date('m');
-for ($i = $start_day_of_cur_month; $i <= $today; ++$i) {
+for ($i = $start_day_of_cur_month; $i <= $today; $i++) {
   $key = $i . '-' . $cur_month;
   $arr[$key] = [0, 0];
 }
@@ -39,7 +39,7 @@ for ($i = $start_day_of_cur_month; $i <= $today; ++$i) {
 $sql = "SELECT DATE_FORMAT(create_at, '%e-%m') as 'day',
   sum(so_luong_da_ban) as tong_da_ban
   from hang_hoa 
-  WHERE DATE(create_at) >= CURDATE() - INTERVAL $max_date DAY
+  WHERE DATE(create_at) > CURDATE() - INTERVAL $max_date DAY
   group by DATE_FORMAT(create_at, '%e-%m')";
 // die($sql);
 $resultSold = mysqli_query($conn, $sql);
@@ -52,7 +52,7 @@ foreach ($resultSold as $each) {
 $sql = "SELECT DATE_FORMAT(create_at, '%e-%m') as 'day',
   sum(so_luong_ton) as tong_ton
   from hang_hoa 
-  WHERE DATE(create_at) >= CURDATE() - INTERVAL $max_date DAY
+  WHERE DATE(create_at) > CURDATE() - INTERVAL $max_date DAY
   group by DATE_FORMAT(create_at, '%e-%m')";
 // die($sql);
 $resultRest = mysqli_query($conn, $sql);
